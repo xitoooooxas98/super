c797ee4fbada6e4f84ef6f869b4b687f3832b8a3
xitoooooxas98 <121181098+xitoooooxas98@users.noreply.github.com>
2022-12-21 12:10:42 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+
+function Nao(){
+     
+    alert("Tem certeza? ")
+    var a = prompt("Digite Sim se esta correto")
+    if (a=== "Sim" ){
+    alert("confere de novo")
+     window.location.reload(false);}
+    else if (a=== "sim"){
+        alert("confere de novo")
+        window.location.reload(false);
+    }else if (a=== "SIM"){
+        alert("confere de novo")
+        window.location.reload(false);
+    }else if (a=== "s1m"){
+        alert("confere de novo")
+        window.location.reload(false);
+    }else if (a=== "siM"){
+        alert("confere de novo")
+        window.location.reload(false);
+    }
+    else if (a=== "SiM"){
+        alert("confere de novo")
+        window.location.reload(false);
+    }
+    else if (a=== "sIm"){
+        alert("confere de novo")
+        window.location.reload(false);
+    }
+}
